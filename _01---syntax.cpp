27760b8b13f05dca09e826144789458825b99199
Vineet Singraul <Singraulvineet20@gmail.com>
2024-11-22 23:41:23 +0530

    first commit

---
+++ _01---syntax.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 2;
+   while (i<=10)
+   {
+     cout<<2*i<<endl;
+     i++;
+   }
+   
+}
---
+++ _02---sum_vala.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int sum = 0;
+ int i=1;
+ while (i<=10)
+ {
+    sum = sum + i ;
+    i++;
+ }
+     cout<<sum;
+
+ 
+    
+}
